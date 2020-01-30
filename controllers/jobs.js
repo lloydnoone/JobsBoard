393c1347a705e557878f0824881f372f7ee43a7d
lloydnoone <lloydnoone92@gmail.com>
2020-01-30 18:34:27 +0000

    clean up

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,7 +1,5 @@
 const axios = require('axios')
-//const Cigar = require('../models/Cigar')
 
-//INDEX
 function index(req, res) {
   axios.all([
     axios.get(`http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${process.env.AZID}&app_key=${process.env.AZKEY}&results_per_page=20&what=${req.params.title}&where=${req.params.location}&content-type=application/json`), //what=javascript%20developer need to regex this
@@ -15,9 +13,7 @@ function index(req, res) {
     })
   ])
     .then(axios.spread((adzunaRes, githubRes, reedRes) => {
-      //map over results array
       const adzunaData = adzunaRes.data.results.map(job => {
-        //for each job, get title
         return ({
           id: job.id,
           title: job.title,
@@ -60,112 +56,8 @@ function index(req, res) {
       res.status(200).send(jobsData)
     }))
     .catch(err => res.status(400).json(err))
-  // Cigar
-  //   .find()
-  //   .populate('user')
-  //   .then(cigars => res.status(200).json(cigars))
-  //   .catch(err => res.status(400).json(err))
 }
 
-//CREATE
-// function create(req, res, next) {
-//   req.body.user = req.currentUser 
-//   console.log('user creating cigar: ', req.currentUser)
-//   Cigar
-//     .create(req.body)
-//     .then(cigar => res.status(201).json(cigar))
-//     .catch(next)
-// }
-
-// //SHOW
-// function show(req, res) {
-//   Cigar
-//     .findById(req.params.id)
-//     .populate('user')
-//     .populate('comments.user')
-//     .then(cigar => {
-//       if (!cigar) return res.status(404).json({ message: 'cigar id not found' })
-//       res.status(200).json(cigar)
-//     })
-//     .catch(err => res.status(400).json(err))
-// }
-
-// //DELETE
-// function destroy(req, res) {
-//   console.log('inside destroy')
-//   Cigar
-//     .findById(req.params.id)
-//     .then(cigar => {
-//       if (!cigar.user.equals(req.currentUser._id)) return res.status(401).json({ message: 'Unauthorized' }) 
-//       return cigar.remove()
-//     })
-//     .then(() => res.sendStatus(204)) 
-//     .catch(err => res.status(400).json(err)) 
-// }
-
-// function update(req, res, next) {
-//   console.log('inside update')
-//   Cigar
-//     .findById(req.params.id)
-//     .then(cigar => {
-//       if (!cigar) return res.status(404).json({ message: 'Not Found' })
-//       if (!cigar.user.equals(req.currentUser._id)) return res.status(401).json({ message: 'Unauthorized' })
-//       return cigar.set(req.body)
-//     })
-//     .then(cigar => cigar.save())
-//     .then(cigar => res.status(202).json(cigar))
-//     .catch(next)
-// }
-
-// //comment create - /cigars/:id/comments
-// function commentCreate(req, res, next) {
-//   req.body.user = req.currentUser
-//   Cigar
-//     .findById(req.params.id)
-//     .then(cigar => {
-//       if (!cigar) return res.status(404).json({ message: 'Not Found' })
-//       console.log('Body is: ', req.body)
-//       cigar.comments.push(req.body)
-//       return cigar.save()
-//     })
-//     .then(cigar => res.status(201).json(cigar))
-//     .catch(next)
-// }
-
-// //comment show - /cigars/:id/comments
-// function commentShow(req, res) {
-//   Cigar
-//     .findById(req.params.id)
-//     .populate('comments.user')
-//     .then(cigar => {
-//       if (!cigar) return res.status(404).json({ message: 'cigar id not found' })
-//       res.status(200).json(cigar.comments)
-//     })
-//     .catch(err => res.status(400).json(err))
-// }
-
-// //comment delete - /cigars/:id/comments/:commentId
-// function commentDelete(req, res) {
-//   Cigar
-//     .findById(req.params.id)
-//     .then(cigar => {
-//       if (!cigar) return res.status(404).json({ message: 'Not Found' })
-//       if (!cigar.user.equals(req.currentUser._id)) return res.status(401).json({ message: 'Unauthorized' })
-//       const comment = cigar.comments.id(req.params.commentId)
-//       comment.remove()
-//       return cigar.save()
-//     })
-//     .then(cigar => res.status(202).json(cigar))
-//     .catch(err => res.json(err))
-// }
-
 module.exports = {
-  index//,
-  //create,
-  //show,
-  // destroy,
-  // update,
-  // commentCreate,
-  // commentDelete,
-  // commentShow
+  index
 }
